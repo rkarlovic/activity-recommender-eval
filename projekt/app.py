3f904da8fb6a63e4fac3870f7e5b1eb19cf4dba2
aolomoao <asmajic@unipu.hr>
2025-07-01 10:00:04 +0200

    Recommendation setup
    updates needed (check for TODO)

---
+++ projekt/app.py
@@ -0,0 +1,87 @@
+from flask import Flask, request, jsonify
+import requests
+import faiss
+import numpy as np
+import os
+import json
+from sentence_transformers import SentenceTransformer
+
+# ~~~~ Postavke modela ~~~~
+embedding_dimension = 1024
+index_file = 'faiss_store.index'
+corpus = "data/corpus.json"
+# TODO: ollama_url = ?
+
+ollama_embedding_url = "TODO"
+ollama_generating_url = "TODO"
+
+app = Flask(__name__)
+
+
+# ~~~~ Učitavanje vector store-a ~~~~
+print("Učitavanje FAISS indexa...")
+index = faiss.read_index(index_file)
+
+with open(corpus, 'r', encoding='utf-8') as file:
+    corpus = json.load(file)
+
+print(f"Učitani korpusi su dužine {len(corpus)} chunkova.")
+
+# ~~~~ Embedding function ~~~~
+def get_embedding(text):
+    payload = {
+        "model": "bge-large", # TODO: replace with chosen model
+        "prompt": [text]
+        # timeout needed? TO CHECK
+    }
+
+    response = requests.post(ollama_embedding_url, json=payload)
+    response.raise_for_status()
+    # TODO: If the response is not successful, it will raise an HTTPError ?
+
+    embedding = response.json(["embedding"])[0]  # emb = response["data"][0]["embedding"] -> "grab the first one only"
+    embedding = np.array(embedding, dtype=np.float32) # standardization to float32
+    
+    faiss.normalize_L2(embedding) # L2 normalization
+    
+    return embedding.reshape(1, -1) # 2D shape (1, n) where n is the embedding size
+
+# ~~~~ Recommendation generator ~~~~
+def generate_recommendation(prompt):
+    payload = {
+        "model" : "", # TODO: update with model name
+        "prompt": prompt
+    }
+
+    response = requests.post(ollama_generating_url, json = payload)
+    response.raise_for_status()
+    return response.json()["response"]
+
+# ~~~~ API endpoint ~~~~
+@app.route("/recommend", methods = ["POST"])
+def create_recommendation():
+    data = request.json
+    tourist_profile = data.get("profile", "")
+    weather = data.get("weather", "")
+
+    query_text = f"{tourist_profile} {weather}" 
+    query_embedding = get_embedding(query_text)
+
+    D, I = index.search(query_embedding, k = 3) # get top 3 recommendations
+    top_chunks = [corpus[i] for i in I[0]]
+
+    prompt = f"Ti si turist koji je dosao na Lošinj. Profil turista: {tourist_profile}, vremenski uvjeti su: {weather}. Preporuči mi aktivnosti na temelju ovih informacija: {top_chunks}."
+
+    rec = generate_recommendation(prompt)
+
+    return jsonify({
+        "profile": tourist_profile,
+        "weather": weather,
+        "faiss_recommendations": top_chunks,
+        "recommendation": rec
+    })
+
+
+# ~~~~ MAIN APP ~~~~
+if __name__ == "__main__":
+    app.run(debug=True)
